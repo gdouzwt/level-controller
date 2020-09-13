f69301010b463fa2586acdd5570d159d794fadf5
招文桃 <gdouzwt@gmail.com>
2020-09-13 13:49:40 +0800

    整理一下代码

--- src/main/java/io/zwt/App.java
+++ src/main/java/io/zwt/App.java
@@ -35,182 +35,172 @@ import static io.zwt.config.Config.*;
 
 public class App extends Application {
 
-    private static volatile String encryptedKey;
-    private static boolean isOn = true;
-    private static final InetSocketAddress UNICAST = new InetSocketAddress(GATEWAY_ADDRESS, PORT);
-    private static final SecretKey SECRET_KEY = new SecretKeySpec(SECRET.getBytes(), 0, 16, "AES");
-    private static DatagramChannel channel = null;
-    private static Button button;
+  private static volatile String encryptedKey;
+  private static boolean isOn = true;
+  private static final InetSocketAddress UNICAST = new InetSocketAddress(GATEWAY_ADDRESS, PORT);
+  private static final SecretKey SECRET_KEY = new SecretKeySpec(SECRET.getBytes(), 0, 16, "AES");
+  private static DatagramChannel channel = null;
+  private static Button button;
 
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
 
-        Selector selector = getSelector();
-        App app = new App();
+    Selector selector = getSelector();
+    App app = new App();
+    Runnable task = new Runnable() {
+      @Override
+      public void run() {
+        doNetwork(selector, app);
+      }
+    };
 
-        Thread backgroundTask = new Thread(
-            () -> {
-                doNetwork(selector, app);
+    Thread backgroundTask = new Thread(task);
+    backgroundTask.setDaemon(true);
+    backgroundTask.start();
+    Application.launch(args);
+  }
+
+  private static void doNetwork(Selector selector, App app) {
+    while (true) {
+      try {
+        if (selector.select(10000) == 0) {
+          System.out.println("Waiting for heartbeat sync...");
+          continue;
+        }
+        for (Iterator<SelectionKey> iterator = selector.selectedKeys().iterator(); iterator.hasNext(); ) {
+          SelectionKey selectionKey = iterator.next();
+          if (selectionKey.isReadable()) {
+            DatagramChannel selectedChannel = (DatagramChannel) selectionKey.channel();
+            DataRecord dataRecord = (DataRecord) selectionKey.attachment();
+            dataRecord.buffer.clear();
+            dataRecord.address = selectedChannel.receive(dataRecord.buffer);
+            if (dataRecord.address != null) {
+              app.onReceiveData(dataRecord.buffer);
+              if (encryptedKey != null) {
+                selectionKey.interestOps(SelectionKey.OP_WRITE);
+              }
             }
-        );
-
-        backgroundTask.setDaemon(true);
-        backgroundTask.start();
-
-        launch(args);
-    }
-
-    private static void doNetwork(Selector selector, App app) {
-        while (true) {
-            try {
-                if (selector.select(10000) == 0) {
-                    System.out.println("Waiting for heartbeat sync...");
-                    continue;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            for (Iterator<SelectionKey> iterator = selector.selectedKeys().iterator(); iterator.hasNext(); ) {
-                SelectionKey selectionKey = iterator.next();
-                if (selectionKey.isReadable()) {
-                    DatagramChannel selectedChannel = (DatagramChannel) selectionKey.channel();
-                    DataRecord dataRecord = (DataRecord) selectionKey.attachment();
-                    dataRecord.buffer.clear();
-                    try {
-                        dataRecord.address = selectedChannel.receive(dataRecord.buffer);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    if (dataRecord.address != null) {
-                        try {
-                            app.onReceiveData(dataRecord.buffer);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        if (encryptedKey != null) {
-                            selectionKey.interestOps(SelectionKey.OP_WRITE);
-                        }
-                    }
-                }
-                if (selectionKey.isValid() && selectionKey.isWritable()) {
-                    if (encryptedKey == null) {
-                        selectionKey.interestOps(SelectionKey.OP_READ);
-                        break;
-                    } else {
-                        selectionKey.interestOps(SelectionKey.OP_READ);
-                    }
-                }
-                iterator.remove();
-            }
-        }
-    }
-
-    private static Selector getSelector() throws IOException {
-        NetworkInterface ni = NetworkInterface.getByName("eth6");
-        InetAddress multicastAddress = InetAddress.getByName(MULTICAST_ADDRESS);
-        channel = DatagramChannel.open(StandardProtocolFamily.INET)
-            .setOption(StandardSocketOptions.SO_REUSEADDR, true)
-            .bind(new InetSocketAddress(PORT))
-            .setOption(StandardSocketOptions.IP_MULTICAST_IF, ni);
-        channel.join(multicastAddress, ni);
-        channel.configureBlocking(false);
-        Selector selector = Selector.open();
-        channel.register(selector, SelectionKey.OP_READ, new DataRecord());
-        return selector;
-    }
-
-    public static void updateRGB(final int cmd, final int colorValue) throws IOException {
-
-        String writeRGBData;
-        if (cmd == -1) {
-            writeRGBData = NEW_RGB_CMD_HEAD + 0 + KEY_JSON_ATTR + encryptedKey + CMD_TRAILER;
-        } else {
-            writeRGBData = NEW_RGB_CMD_HEAD + ((0xFFL << 24) | colorValue) + KEY_JSON_ATTR + encryptedKey + CMD_TRAILER;
-        }
-        ByteBuffer to = ByteBuffer.wrap(writeRGBData.getBytes());
-        if (encryptedKey != null) {
-            channel.send(to, UNICAST);
-            System.out.println(writeRGBData);
-        }
-    }
-
-    public static void togglePlug() throws IOException {
-        isOn = !isOn;
-        String writePlugData = WRITE_PLUG + (isOn ? OFF : ON) + KEY_JSON_ATTR + encryptedKey + CMD_TRAILER;
-        ByteBuffer to = ByteBuffer.wrap(writePlugData.getBytes());
-        System.out.println(writePlugData);
-        if (encryptedKey != null) {
-            channel.send(to, UNICAST);
-        }
-    }
-
-    private synchronized String onReceiveData(Buffer input) throws Exception {
-
-        input.flip();
-        byte[] content = new byte[input.limit()];
-        while (input.hasRemaining()) {
-            content[input.position()] = ((ByteBuffer) input).get();
-        }
-        String stringContent = new String(content);
-        // find token and encrypt it
-        if (stringContent.contains("gateway") && stringContent.contains("heartbeat")) {
-            int token = stringContent.indexOf("token");
-            token += 8;
-            String tokenString = stringContent.substring(token, token + 16);
-            byte[] cipher = encryptToken(tokenString);
-            encryptedKey = DatatypeConverter.printHexBinary(cipher);
-            System.out.println(KEY_UPDATED);
-        }
-        System.out.println(stringContent);
-        if (stringContent.contains("report") && stringContent.contains("plug")) {
-            if (stringContent.contains("on")) {
-                Platform.runLater(() -> {
-                    button.setBackground(new Background(new BackgroundFill(Paint.valueOf("green"), new CornerRadii(12), null)));
-                    button.setTextFill(Paint.valueOf("white"));
-                    button.setText("插座已开");
-                });
-
+          }
+          if (selectionKey.isValid() && selectionKey.isWritable()) {
+            if (encryptedKey == null) {
+              selectionKey.interestOps(SelectionKey.OP_READ);
+              break;
             } else {
-                Platform.runLater(() -> {
-                    button.setBackground(new Background(new BackgroundFill(Paint.valueOf("grey"), new CornerRadii(12), null)));
-                    button.setTextFill(Paint.valueOf("black"));
-                    button.setText("插座已关");
-                });
+              selectionKey.interestOps(SelectionKey.OP_READ);
             }
+          }
+          iterator.remove();
         }
-        if (stringContent.contains("Invalid key")) {
-            encryptedKey = null;
-        }
-        return stringContent;
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
+  }
 
-    private static byte[] encryptToken(String token) throws Exception {
-        return SymmetricEncryption.performAESEncryption(token, SECRET_KEY, INITIALIZATION_VECTOR);
-    }
+  private static Selector getSelector() throws IOException {
+    NetworkInterface ni = NetworkInterface.getByName("eth6");
+    InetAddress multicastAddress = InetAddress.getByName(MULTICAST_ADDRESS);
+    channel = DatagramChannel.open(StandardProtocolFamily.INET)
+      .setOption(StandardSocketOptions.SO_REUSEADDR, true)
+      .bind(new InetSocketAddress(PORT))
+      .setOption(StandardSocketOptions.IP_MULTICAST_IF, ni);
+    channel.join(multicastAddress, ni);
+    channel.configureBlocking(false);
+    Selector selector = Selector.open();
+    channel.register(selector, SelectionKey.OP_READ, new DataRecord());
+    return selector;
+  }
 
-    @Override
-    public void stop() throws Exception {
-        System.out.println("Saving preference...");
-        try {
-            Properties properties = new Properties();
-            properties.setProperty("lamp.status", String.valueOf(AppController.status));
-            properties.setProperty("lamp.color", AppController.color);
-            URL resource = getClass().getClassLoader().getResource("preference.properties");
-            FileWriter fileWriter = new FileWriter(Paths.get(resource.toURI()).toFile());
-            properties.store(fileWriter, "lamp");
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        }
+  public static void updateRGB(final int cmd, final int colorValue) throws IOException {
+    String writeRGBData;
+    if (cmd == -1) {
+      writeRGBData = NEW_RGB_CMD_HEAD + 0 + KEY_JSON_ATTR + encryptedKey + CMD_TRAILER;
+    } else {
+      writeRGBData = NEW_RGB_CMD_HEAD + ((0xFFL << 24) | colorValue) + KEY_JSON_ATTR + encryptedKey + CMD_TRAILER;
     }
+    ByteBuffer to = ByteBuffer.wrap(writeRGBData.getBytes());
+    if (encryptedKey != null) {
+      channel.send(to, UNICAST);
+      System.out.println(writeRGBData);
+    }
+  }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent parent = FXMLLoader.load(getClass().getResource("/fxml/main-pane.fxml"), ResourceBundle.getBundle("preference"));
-        button = (Button) parent.lookup("#button");
-        button.setBackground(new Background(new BackgroundFill(Paint.valueOf("grey"), new CornerRadii(12), null)));
-        Scene scene = new Scene(parent);
-        //scene.getStylesheets().add(getClass().getResource("/fxml/style.css").toString());
-        stage.setScene(scene);
-        stage.setTitle(APP_TITLE);
-        stage.show();
+  public static void togglePlug() throws IOException {
+    isOn = !isOn;
+    String writePlugData = WRITE_PLUG + (isOn ? OFF : ON) + KEY_JSON_ATTR + encryptedKey + CMD_TRAILER;
+    ByteBuffer to = ByteBuffer.wrap(writePlugData.getBytes());
+    System.out.println(writePlugData);
+    if (encryptedKey != null) {
+      channel.send(to, UNICAST);
     }
+  }
+
+  private synchronized String onReceiveData(Buffer input) throws Exception {
+    input.flip();
+    byte[] content = new byte[input.limit()];
+    while (input.hasRemaining()) {
+      content[input.position()] = ((ByteBuffer) input).get();
+    }
+    String stringContent = new String(content);
+    // find token and encrypt it
+    if (stringContent.contains("gateway") && stringContent.contains("heartbeat")) {
+      int token = stringContent.indexOf("token");
+      token += 8;
+      String tokenString = stringContent.substring(token, token + 16);
+      byte[] cipher = encryptToken(tokenString);
+      encryptedKey = DatatypeConverter.printHexBinary(cipher);
+      System.out.println(KEY_UPDATED);
+    }
+    System.out.println(stringContent);
+    if (stringContent.contains("report") && stringContent.contains("plug")) {
+      if (stringContent.contains("on")) {
+        Platform.runLater(() -> {
+          button.setBackground(new Background(new BackgroundFill(Paint.valueOf("green"), new CornerRadii(12), null)));
+          button.setTextFill(Paint.valueOf("white"));
+          button.setText("插座已开");
+        });
+      } else {
+        Platform.runLater(() -> {
+          button.setBackground(new Background(new BackgroundFill(Paint.valueOf("grey"), new CornerRadii(12), null)));
+          button.setTextFill(Paint.valueOf("black"));
+          button.setText("插座已关");
+        });
+      }
+    }
+    if (stringContent.contains("Invalid key")) {
+      encryptedKey = null;
+    }
+    return stringContent;
+  }
+
+  private static byte[] encryptToken(String token) throws Exception {
+    return SymmetricEncryption.performAESEncryption(token, SECRET_KEY, INITIALIZATION_VECTOR);
+  }
+
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    Parent parent = FXMLLoader.load(getClass().getResource("/fxml/main-pane.fxml"), ResourceBundle.getBundle("preference"));
+    button = (Button) parent.lookup("#button");
+    button.setBackground(new Background(new BackgroundFill(Paint.valueOf("grey"), new CornerRadii(12), null)));
+    Scene scene = new Scene(parent);
+    //scene.getStylesheets().add(getClass().getResource("/fxml/style.css").toString());
+    stage.setScene(scene);
+    stage.setTitle(APP_TITLE);
+    stage.show();
+  }
+
+  @Override
+  public void stop() throws Exception {
+    System.out.println("Saving preference...");
+    try {
+      Properties properties = new Properties();
+      properties.setProperty("lamp.status", String.valueOf(AppController.status));
+      properties.setProperty("lamp.color", AppController.color);
+      URL resource = getClass().getClassLoader().getResource("preference.properties");
+      FileWriter fileWriter = new FileWriter(Paths.get(resource.toURI()).toFile());
+      properties.store(fileWriter, "lamp");
+    } catch (IOException ioException) {
+      ioException.printStackTrace();
+    }
+  }
 }
